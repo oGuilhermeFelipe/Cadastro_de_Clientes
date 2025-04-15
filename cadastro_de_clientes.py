63664a532a377c72090b71f6b07ef879cb53f2c3
oGuilhermeFelipe <guilhermeflipe01@gmail.com>
2025-04-14 21:25:48 -0300

    Add files via upload

---
+++ cadastro_de_clientes.py
@@ -0,0 +1,22 @@
+# Lista para armazenar os cadastros 
+pessoas = []
+
+while True:
+    print("\nCadastro de pessoa")
+    nome = input("Nome: ")
+    idade = int(input("Idade: "))
+    cidade = input("Cidade onde nasceu: ")
+
+    #Adicionando a pessoa como dicionário na lista
+    pessoa = {"nome": nome, "idade": idade, "cidade": cidade}
+    pessoas.append(pessoa)
+
+    #Perguntar se quer continuar 
+    continuar = input("Deseja cadastrar outra pessoa? (s/n): ").strip().lower()
+    if continuar != "s":
+        break
+
+#Exibindo os cadastros 
+print("\nPessoas cadastradas")
+for i, pessoa in enumerate(pessoas, start=1):
+    print(f"{i}. Nome: {pessoa['nome']}, Idade:{pessoa['idade']}, Cidade: {pessoa['cidade']}")
